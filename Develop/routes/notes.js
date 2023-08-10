6b55ab2ea4b461529a0a70dd23d4b41b5e0955ff
Devarasen <devarasen@gmail.com>
2023-08-10 14:49:30 +1000

    delete functionality added

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,7 +1,9 @@
 const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const path = require('path');
 
 //Import helper functions
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils');
 
 // GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
@@ -9,17 +11,17 @@ notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-
 // POST Route for notes
 notes.post('/', (req, res) => {
     console.info(`${req.method} request received to add a tip`);
   
-    const { title, text } = req.body;
+    const { title, text, note_id } = req.body;
   
     if (req.body) {
       const newNote = {
         title,
-        text
+        text,
+        note_id: uuidv4(),
       };
   
       readAndAppend(newNote, './db/db.json');
@@ -27,6 +29,36 @@ notes.post('/', (req, res) => {
     } else {
       res.error('Error in adding note');
     }
+});
+
+// DELETE route to delete a note by ID
+notes.delete('/:id', (req, res) => {
+  const idToDelete = req.params.id;
+
+  readFromFile(path.join(__dirname, '../db/db.json'))
+    .then(data => {
+      let notes = JSON.parse(data);
+
+      console.log('Notes before deletion:', notes);
+
+      const noteIndex = notes.findIndex(note => note.note_id === idToDelete);
+
+      console.log('Note Index to delete:', noteIndex);
+
+      if (noteIndex === -1) {
+        return res.status(404).json({ error: 'Note not found' });
+      }
+
+      notes.splice(noteIndex, 1);
+
+      writeToFile(path.join(__dirname, '../db/db.json'), notes);
+
+      return res.status(204).send();
+    })
+    .catch(err => {
+      console.error('Error:', err);
+      return res.status(500).json({ error: 'Error reading or writing to the database file' });
+    });
 });
 
 module.exports = notes;
--- Develop/server.js
+++ Develop/server.js
@@ -23,12 +23,12 @@ app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // Wildcard route to direct users to index.html
@@ -37,5 +37,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
+    console.log(`App listening at http://localhost:${PORT}`)
 );
